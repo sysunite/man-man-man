db99b5500ed2ee04ea4ed64c0ee2852b1d6ccd89
Mohamad Alamili <mohamad@sysunite.com>
2016-02-05 23:35:54 +0100

    Added sound

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+process.title = 'manmanman';
+
+var print = function() {
+  process.stdout.write("man ");
+  setTimeout(print, 100);
+};
+
+var player = require('play-sound')(opts = {});
+var file = require('path').resolve(__dirname, 'rob.mp3');
+var play = function() {
+  player.play(file, play);
+};
+
+print();
+play();
